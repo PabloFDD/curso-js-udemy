58fa7587622d7f0889acd5f68a4f1a8da710c2a0
PabloFDD <pablofdelboni@gmail.com>
2022-10-03 10:40:12 -0300

    Calculadora feita com factory function

--- Curso Javascript/Seção 4 - Funções/assets/js/calculadora.js
+++ Curso Javascript/Seção 4 - Funções/assets/js/calculadora.js
@@ -1,5 +1,67 @@
 (() => {
+//------------------------------------------- Utilizando Factory Function --------------------------------
+function criaCalculadora(){
+  return {
+    display: document.querySelector('.display'),
+    
+    inicia(){
+      this.cliqueBotoes();
+    },
 
+    realizaConta(){
+      let conta = this.display.value;
 
+      try{
+        conta = eval(conta);
+
+        if(!conta){
+          alert('Conta inválida');
+          return;
+        }
+        this.display.value = String(conta);
+      }catch(e){
+        alert('Conta inválida');
+        return;
+      }
+    },
+
+    clearDisplay(){
+      this.display.value = '';
+    },
+    
+    apagaUltimo(){
+      this.display.value = this.display.value.slice(0, -1);
+    },
+
+    cliqueBotoes(){
+      document.addEventListener('click', (e) =>{
+        const el = e.target;
+
+        if(el.classList.contains('btn-num')){
+          this.btnParaDisplay(el.innerText);
+        }
+
+        if(el.classList.contains('btn-clear')){
+          this.clearDisplay();
+        }
+
+        if(el.classList.contains('btn-del')){
+          this.apagaUltimo();
+        }
+
+        if(el.classList.contains('btn-eq')){
+          this.realizaConta();
+        }
+      });
+    },
+    btnParaDisplay(valor){
+      this.display.value += valor;
+    }
+
+  };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
   
 })();
